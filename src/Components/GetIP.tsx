59be0ef1de417d6a51523440ebdc49abb63e2abe
gabrielforster <rochafrgabriel@gmail.com>
2022-03-15 22:50:18 -0300

    Initial commit

---
+++ src/Components/GetIP.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+export const GetIP = () => {
+
+
+
+  return (
+    <main className='container'>
+        <div className="getip">
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+        </div>
+    </main>
+  )
+}
